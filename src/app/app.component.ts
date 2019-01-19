b96119a5294f607d8b45d8513036382f542f4253
Ranjan <jyotiranjan.p14@gmail.com>
2019-01-19 18:43:25 +0530

    Getting Started Angular 19 Jan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  //selector: 'test-root',
+  // selector: '[test-root]',
+  selector: '.test-root',
+  //template:'<h1>Inline Template Example</h1>',
+  // template:`
+  // <h1>Inline Template Example 2</h1>
+  // <h2>This example is for adding multiple line of HTML in template property</h2>
+  // `,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styles:[
+  //   '.myClass1{background-color: yellow;}',
+  //   `.myClass2{
+  //     background-color: blue;
+    
+  //   }`
+  // ]
+  styleUrls:['./app.component.css']
 })
 export class AppComponent {
-  title = 'WeekendAngJan';
+  title:string="Weekend Angular January";
 }
